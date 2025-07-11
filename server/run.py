9ac8795f5e2ee9adb6cad59b295b881e27b2eacd
Vidya Bharti <vidyab5058@gmail.com>
2025-07-11 15:16:21 +0530

    Add text processing with spaCy, enhance React frontend, Add config file, and logging

--- server/run.py
+++ server/run.py
@@ -75,5 +75,4 @@ def upload_file():
     return 'Error uploading file', 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
